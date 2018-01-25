ab7fbad5d190bc6730675d1066ecdc25e6f65696
sdlm <sdlmerr@yandex.ru>
2018-01-25 21:35:52 +0300

    add pages

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-import routes from './routes';
+// import routes from './routes';
+import fs from 'fs';
 
-// 4. Create and mount the root instance.
+const home_page_template = { template: fs.readFileSync(__dirname + '/templates/home.html', 'utf8') };
+const start_page_template = { template: fs.readFileSync(__dirname + '/templates/start.html', 'utf8') };
+const deposit_page_template = { template: fs.readFileSync(__dirname + '/templates/deposit.html', 'utf8') };
+const credit_page_template = { template: fs.readFileSync(__dirname + '/templates/credit.html', 'utf8') };
+const flat_page_template = { template: fs.readFileSync(__dirname + '/templates/flat.html', 'utf8') };
+const result_page_template = { template: fs.readFileSync(__dirname + '/templates/result.html', 'utf8') };
+
+const routes = [
+  { path: '/', component: home_page_template },
+  { path: '/start', component: start_page_template },
+  { path: '/deposit', component: deposit_page_template },
+  { path: '/credit', component: credit_page_template },
+  { path: '/flat', component: flat_page_template },
+  { path: '/result', component: result_page_template },
+];
+
+// init app
 const app = new Vue({
   el: '#app',
-  router: new VueRouter({routes: routes}),
+  router: new VueRouter({routes}),
 });
--- routes.js
+++ routes.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
-const home_page_template = { template: fs.readFileSync(__dirname + '/templates/home_page.html', 'utf8') };
-const start_page_template = { template: fs.readFileSync(__dirname + '/templates/start_page.html', 'utf8') };
+const home_page_template = { template: fs.readFileSync(__dirname + '/templates/home.html', 'utf8') };
+const start_page_template = { template: fs.readFileSync(__dirname + '/templates/start.html', 'utf8') };
 
 const routes = [
   { path: '/', component: home_page_template },
